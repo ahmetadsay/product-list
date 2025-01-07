47ee69bd3abc63bfa8cc307a17c0777ca2b84fe6
ahmet <adsayahmet94@hotmail.com>
2025-01-08 01:33:44 +1100

    added delete functionality and styled css

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,62 @@ function App() {
   const [cart, setCart] = useState([]);
 
   const handleAddToCart = (product) => {
-    const existingProduct = cart.find(item => item.product.name === product.name);
+    const existingProduct = cart.find(
+      (item) => item.product.name === product.name
+    );
     if (existingProduct) {
-      setCart(cart.map(item =>
-        item.product.name === product.name
-          ? { ...item, quantity: item.quantity + 1, total: (item.quantity + 1) * item.product.price }
-          : item
-      ));
+      setCart(
+        cart.map((item) =>
+          item.product.name === product.name
+            ? {
+                ...item,
+                quantity: item.quantity + 1,
+                total: (item.quantity + 1) * item.product.price,
+              }
+            : item
+        )
+      );
     } else {
       setCart([...cart, { product, quantity: 1, total: product.price }]);
     }
   };
 
+  const removeItem = (product) => {
+    const existingProduct = cart.find(
+      (item) => item.product.name === product.name
+    );
+    if (existingProduct.quantity === 1) {
+      setCart(cart.filter((item) => item.product.name !== product.name));
+    } else {
+      setCart(
+        cart.map((item) =>
+          item.product.name === product.name
+            ? {
+                ...item,
+                quantity: item.quantity - 1,
+                total: (item.quantity - 1) * item.product.price,
+              }
+            : item
+        )
+      );
+    }
+  }
+
+ 
   return (
     <div>
       <h1 className=" font-bold text-2xl my-4">Desserts</h1>
       <div className=" flex flex-col sm:flex-row ">
         <div className="flex flex-wrap">
           {products.map((product, index) => (
-            <ProductCard onAddToCart={handleAddToCart} key={index} product={product} />
+            <ProductCard
+              onAddToCart={handleAddToCart}
+              key={index}
+              product={product}
+            />
           ))}
         </div>
-        <Cart cart={cart} />
+        <Cart onRemoveItem={removeItem}  cart={cart} />
       </div>
     </div>
   );
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,27 +1,43 @@
 import React from "react";
 import emptyCard from "../../assets/images/illustration-empty-cart.svg";
+import deleteButton from "../../assets/images/icon-remove-item.svg";
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, onRemoveItem }) => {
   const totalAmount = cart.reduce((acc, item) => acc + item.total, 0);
   console.log(cart);
 
   return (
     <>
       {cart.length ? (
-        <div className="p-4 border border-gray-300 rounded-lg">
-          <h2 className="text-lg font-semibold mb-4">Your Cart</h2>
-          <ul>
+        <div className="p-4 min-w-96 h-1/4 bg-white border rounded-lg">
+          <h2 className="text-lg text-rose-900 font-semibold mb-4">
+            Your Cart ({cart.length})
+          </h2>
+          <ul className="space-y-2">
             {cart.map((item, index) => (
-              <li key={index} className="mb-2">
-                <div className="flex justify-between">
+              <li
+                key={index}
+                className="flex items-center justify-between mb-2"
+              >
+                <div className="flex flex-col">
                   <span>
                     {item.product.name} x {item.quantity}
                   </span>
                   <span>${item.total.toFixed(2)}</span>
+                  <span className="text-gray-400 text-sm">
+                    {item.product.category}
+                  </span>
+                  <span className="text-gray-400 text-sm">
+                    ${item.product.price}
+                  </span>
                 </div>
+                <button onClick={() => onRemoveItem(item.product)} className="ml-4 flex items-center justify-center w-8 h-8 rounded-full border border-rose-900 hover:bg-gray-100">
+                  <img src={deleteButton} alt="Remove" className="w-4 h-4 " />
+                </button>
               </li>
             ))}
           </ul>
+
           <div className="mt-4 border-t pt-4">
             <div className="flex justify-between font-semibold">
               <span>Total</span>
